f50f15a82a62d3f47835782ecfcfe7a9ec528750
victor martinez <vitico841@gmail.com>
2025-02-11 12:09:33 -0400

    Añade soporte para microservicios utilizando NATS.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const consoleLogger = new Logger('Main', { timestamp: true });
+  const contex = await NestFactory.createApplicationContext(AppModule);
+  const configService = contex.get(ConfigService);
+  const NATS_URL = configService.get<string>('NATS_URL');
+  await contex.close();
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: [NATS_URL],
+      },
+    },
+  );
+  consoleLogger.log('Email Microservice running in Nats');
+  await app.listen();
 }
 bootstrap();
